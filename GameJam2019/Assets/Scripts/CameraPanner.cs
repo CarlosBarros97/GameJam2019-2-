457f00cc3df9abf7659ef48a6f6dc98074713222
kizido <kieranking3555@gmail.com>
2019-09-28 12:11:26 -0700

    camera pan mechanic

---
+++ GameJam2019/Assets/Scripts/CameraPanner.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPanner : MonoBehaviour
+{
+
+    public Camera mainCam;
+
+    public float travelSpeed = 1f;
+
+    private Vector3[] camLocations = new[] { new Vector3(0f, 0f, -10f), new Vector3(16f, 0f, -10f) };
+    private int currentCam = 0;
+
+    private float step;
+    private bool camMoving = false;
+
+    private void Start()
+    {
+        mainCam.transform.position = camLocations[0];
+
+        step = travelSpeed * Time.deltaTime;
+    }
+
+    private void Update()
+    {
+        moveToNextCam();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "CamPanCollider")
+        {
+            collision.collider.enabled = false;
+            camMoving = true;
+        }
+
+    }
+
+    private void moveToNextCam()
+    {
+        if (camMoving)
+        {
+            mainCam.transform.position = Vector3.MoveTowards(mainCam.transform.position, camLocations[currentCam + 1], step);
+            Debug.Log("MOVING CAM");
+
+            if(Vector3.Distance(mainCam.transform.position, camLocations[currentCam + 1]) < 0.01f)
+            {
+                camMoving = false;
+                currentCam++;
+            }
+        }
+    }
+
+
+
+}
