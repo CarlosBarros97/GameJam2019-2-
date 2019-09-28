bbcc4c9b4402e614994e8b331e50172a0af07029
kizido <kieranking3555@gmail.com>
2019-09-28 10:28:33 -0700

    Added timer/gameover/gamestart

---
+++ GameJam2019/Assets/Scripts/GameTimer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameTimer : MonoBehaviour
+{
+
+    public Text timerText;
+    private float startTime = 150f;
+    private float currentTime;
+
+    private void Start()
+    {
+        currentTime = startTime;
+    }
+
+    private void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+
+        string minutes = ((int)currentTime / 60).ToString();
+        string seconds = (currentTime % 60).ToString("f0");
+
+        timerText.text = minutes + ":" + seconds;
+
+        if (currentTime <= 0)
+        {
+            currentTime = 0;
+
+            // Load game over UI
+        }
+    }
+
+    // Instantiate game over UI
+    public void GameOver()
+    {
+    }
+
+    // Allow other classes to remove time from the timer
+    public void removeTime(float timeRemoved)
+    {
+        currentTime -= timeRemoved;
+    }
+
+
+    // ADD METHOD THAT CHANGES COLOR OF TIMER TEXT WHEN LOW ON TIME
+
+
+}
---
+++ GameJam2019/Assets/Scripts/StartGame.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class StartGame : MonoBehaviour
+{
+
+    // Name of scene to load for beginning of game
+    private string sceneToLoad = "GameScene1";
+
+    // Start the game on button press
+    public void startGame()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
