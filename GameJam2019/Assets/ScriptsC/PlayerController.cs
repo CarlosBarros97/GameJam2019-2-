9508d801698c451484e8d3871dcd0ab02d69f680
Carlos_Barros97 <kadumbarros@gmail.com>
2019-09-28 10:22:53 -0700

    Added player movement

---
+++ GameJam2019/Assets/ScriptsC/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 3f;
+    public float jumpForce = 200f;
+    public Rigidbody2D rb;
+    public bool isGround = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Horizontal"))
+        {
+            transform.position += new Vector3(moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, 0f);
+            {
+
+            }
+        }
+        if (Input.GetButton("Jump")&&isGround)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce));
+            isGround = false;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isGround = true;
+    }
+}
