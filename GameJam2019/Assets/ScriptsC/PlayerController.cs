0d19d913610163a44b34aef9bc221e182fff489f
kizido <kieranking3555@gmail.com>
2019-09-28 12:53:36 -0700

    added walk animations and sprite flipping

--- GameJam2019/Assets/ScriptsC/PlayerController.cs
+++ GameJam2019/Assets/ScriptsC/PlayerController.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // Animation variables
+    public Animator playerAnimator;
+    private bool isMoving = false;
+
+
+    // Other variables
     public float moveSpeed = 3f;
     public float jumpForce = 200f;
     public Rigidbody2D rb;
     public bool isGround = true;
+    private SpriteRenderer playerSprite;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        playerSprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -20,10 +28,23 @@ public class PlayerController : MonoBehaviour
         if (Input.GetButton("Horizontal"))
         {// Move player if arrow keys are pressed
             transform.position += new Vector3(moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, 0f);
-            {
+            isMoving = true;
 
+            if(Input.GetAxis("Horizontal") < 0)
+            {
+                playerSprite.flipX = true;
             }
-        } // Jump if spacebar is pressed and checks if player has already jumped
+            else if(Input.GetAxis("Horizontal") > 0) {
+                playerSprite.flipX = false;
+            }
+        }
+        else
+        {
+            isMoving = false;
+        }
+        playerAnimator.SetBool("isMoving", isMoving);
+
+        // Jump if spacebar is pressed and checks if player has already jumped
         if (Input.GetButton("Jump")&&isGround)
         {
             rb.AddForce(new Vector2(0f, jumpForce));
